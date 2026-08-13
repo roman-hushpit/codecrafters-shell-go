8a5769d01e1b0fc12cefdc0931b54af2cb6cdcca
Roman <roman.gushpit@gmail.com>
2026-08-13 20:43:45 +0000

    codecrafters submit [skip ci]

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -7,9 +7,24 @@ import (
 
 type Builtins map[string]func() error
 
-var BuiltinsMap = map[string]func(args ...string) error{
-	"exit": exitFunc,
-	"echo": echoFunc,
+var BuiltinsMap map[string]func(args ...string) error
+
+func init() {
+	BuiltinsMap = map[string]func(args ...string) error{
+		"exit": exitFunc,
+		"echo": echoFunc,
+		"type": typeFunc,
+	}
+}
+
+func typeFunc(args ...string) error {
+	functionName := args[0]
+	if _, ok := BuiltinsMap[functionName]; ok {
+		fmt.Printf("%s is a shall builtin\n", functionName)
+		return nil
+	}
+	fmt.Printf("%s: not found\n", functionName)
+	return nil
 }
 
 func echoFunc(args ...string) error {
