35cfc9adc5d122bfb1f0d31cd8a8876d2d0d5b7d
Roman <roman.gushpit@gmail.com>
2026-08-31 15:22:00 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,26 +1,51 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+	"io"
+	"strings"
 
+	"github.com/chzyer/readline"
 	"github.com/codecrafters-io/shell-starter-go/executor"
 	"github.com/codecrafters-io/shell-starter-go/parser"
 	"github.com/codecrafters-io/shell-starter-go/processor"
 )
 
 func main() {
+
+	var completer = readline.NewPrefixCompleter(
+		readline.PcItem("echo"),
+		readline.PcItem("exit"),
+	)
+
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:          "$ ",
+		AutoComplete:    completer,
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	l.CaptureExitSignal()
+
 	for {
-		fmt.Print("$ ")
-		input := bufio.NewScanner(bufio.NewReader(os.Stdin))
-		if input.Scan() {
-			userCommand := input.Text()
-			newParser := parser.NewParser()
-			newParser.Parse(userCommand)
-			arguments := newParser.Args()
-			command := processor.ProcessCommandArgs(arguments)
-			executor.ExecuteCommand(command)
+		line, lineError := l.Readline()
+		if lineError == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if lineError == io.EOF {
+			break
 		}
+		line = strings.TrimSpace(line)
+		newParser := parser.NewParser()
+		newParser.Parse(line)
+		arguments := newParser.Args()
+		command := processor.ProcessCommandArgs(arguments)
+		executor.ExecuteCommand(command)
+
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/codecrafters-io/shell-starter-go
 
 go 1.26.0
+
+require github.com/chzyer/readline v1.5.1 // indirect
+
+require golang.org/x/sys v0.47.0 // indirect
